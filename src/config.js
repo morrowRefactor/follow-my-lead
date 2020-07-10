e21ba402266d785002f2099006de9f238dc69d9e
Kurt Morrow <kurt.morrow@gmail.com>
2020-07-10 11:51:06 -0700

    updated api endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://dry-dusk-63405.herokuapp.com/',
+    API_ENDPOINT: 'https://dry-dusk-63405.herokuapp.com',
     GOOGLE_API_KEY: 'AIzaSyDFFvmBKhgEPjsrftIYez2j2ZrfyJ7xEeU'
 }
