c6c01130fc953def6620d0f48b9eb20f587d94dc
Kurt Morrow <kurt.morrow@gmail.com>
2021-03-09 14:27:55 -0800

    updated navbar styling and functionality

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,18 +6,34 @@ import './NavBar.css';
 class Navbar extends Component {
     static contextType = APIContext;
 
+    handleMenuToggle = () => {
+        if(window.innerWidth < 1200) {
+            this.context.toggleNav()
+        }
+    };
+
+    homeMenuToggle = () => {
+        if(this.context.navbar === 'show') {
+          this.context.toggleNav();
+        }
+    };
+
     render() {
         return (
-            <nav className="TopNav">
+            <section className="TopNav">
                 <img className='topNavLogo' src='https://user-images.githubusercontent.com/58446465/88106759-d63c6980-cb5a-11ea-80c4-25caf6b14855.png' alt='Follow My Lead Logo' />
-                <div className="mainTitle"><Link className="navTitle" to='/'>Follow My Lead</Link></div>
+                <div className="mainTitle"><Link className="navTitle" to='/' onClick={() => {this.homeMenuToggle()}}>Follow My Lead</Link></div>
                 <div className="hamburger"><button className='navHamburger' onClick={() => {this.context.toggleNav()}}>&#9776;</button></div>
                 <div className="break"></div>
-                <ul className={this.context.navbar}>
-                    <li><Link className='navLink' to='/browse-routes'>Browse Routes</Link></li>
-                    <li><Link className='navLink' to='/create-route'>Create Route</Link></li>
-                </ul>
-            </nav>
+                <section className={this.context.navbar}>
+                  <section className='TopNav_contentContainer'>
+                    <ul>
+                        <li><Link className='navLink' to='/browse-routes' onClick={() => {this.handleMenuToggle()}}>Browse Routes</Link></li>
+                        <li><Link className='navLink' to='/create-route' onClick={() => {this.handleMenuToggle()}}>Create Route</Link></li>
+                    </ul>
+                  </section>
+                </section>
+            </section>
         );
     };
 };
